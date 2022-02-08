3af2f00e5dd3896cc1b7fae6466f79d15d8e800d
Anais Augu <augu.anais@gmail.com>
2022-02-08 17:04:02 +0100

    restaurants and reviews routes added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,6 @@ class Restaurant < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :phone_number, presence: true
   validates :category,
     inclusion: {
       in: %w(chinese italian japanese french belgian),
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating restaurants..."
+restaurants = [Restaurant.new(name: "Sushizen", address: "Grancy", category: "japanese"),
+  Restaurant.new(name: "Dieci", address: "Pully", category: "italian"),
+  Restaurant.new(name: "Les brasseurs", address: "St Francois", category: "french"),
+  Restaurant.new(name: "Belgia", address: "Marterey", category: "belgian"),
+  Restaurant.new(name: "Zoo Burger", address: "Marterey", category: "belgian")
+]
+
+puts "Saving restaurants..."
+restaurants.each { |restaurant| restaurant.save }
+puts "Restaurants created !"
